083a8c2742d66e8f80840f3aa57e966fb27d91b9
Serueja <serueja@yandex.ru>
2022-06-01 19:09:55 +0300

    frontend added, everything is ok

---
+++ prognosis_frontend/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Month_first from './components/Month/Month_first';
+
+
+
+const HomePage = (props) => {
+    return (
+     <div className='app-wrapper' >
+        <Header />
+        <Navbar />
+    </div>
+    )
+} 
+const App = (props) => {
+
+    return <BrowserRouter>
+                <Routes>
+                <Route exact path="/"
+                        element={<HomePage />} />
+
+                    <Route  path="/:id"
+                        element={<Month_first />} />      
+                </Routes>
+    </BrowserRouter>
+
+   
+}
+
+export default App;
